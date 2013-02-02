2b36316a6092627214cd59f5e4085f3a76c6ec98
behnam <behnam_bas@hotmail.com>
2013-02-02 13:47:33 -0800

    added account status to view

--- application/views/gl/manage_accounts.php
+++ application/views/gl/manage_accounts.php
@@ -6,6 +6,8 @@ echo form_label('ID', 'id');
 echo form_input('id', '{id}');
 echo form_label('Account Name', 'account_name');
 echo form_input('account_name', '{account_name}');
+echo form_label('Account Date', 'account_date');
+echo form_input('account_date', '{account_date}');
 echo form_label('Account Status', 'account_status');
 echo form_input('account_status', '{account_status}');
 echo form_submit('', 'Submit' );
