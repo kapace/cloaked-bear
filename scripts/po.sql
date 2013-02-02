ad97bcfb400963d037d7a9948fa3c25cd2e12e04
David Czech <davidczech@acm.org>
2013-02-02 13:15:29 -0800

    added some theme to products

--- scripts/po.sql
+++ scripts/po.sql
@@ -11,15 +11,15 @@ CREATE TABLE purchases
 `supplierno` VARCHAR(4) NOT NULL
 );
 
-INSERT INTO purchases VALUES ('001', 'abc', 'A', '2012-01-01', 100, 20, 100, '1234');
-INSERT INTO purchases VALUES ('002', 'bcd', 'A', '2012-01-01', 100, 20, 100, '1234');
-INSERT INTO purchases VALUES ('003', 'cde', 'D', '2012-01-01', 100, 20, 100, '2345');
-INSERT INTO purchases VALUES ('004', 'def', 'A', '2012-01-01', 100, 20, 100, '2345');
-INSERT INTO purchases VALUES ('005', 'efg', 'D', '2012-01-01', 100, 20, 100, '2345');
-INSERT INTO purchases VALUES ('006', 'fgh', 'A', '2012-01-01', 100, 20, 100, '3456');
-INSERT INTO purchases VALUES ('007', 'ghi', 'A', '2012-01-01', 100, 20, 100, '3456');
-INSERT INTO purchases VALUES ('008', 'hij', 'D', '2012-01-01', 100, 20, 100, '1234');
-INSERT INTO purchases VALUES ('009', 'ijk', 'D', '2012-01-01', 100, 20, 100, '1234');
-INSERT INTO purchases VALUES ('010', 'jkl', 'A', '2012-01-01', 100, 20, 100, '1234');
-INSERT INTO purchases VALUES ('011', 'klm', 'A', '2012-01-01', 100, 20, 100, '2345');
-INSERT INTO purchases VALUES ('012', 'lmn', 'A', '2012-01-01', 100, 20, 100, '3456');
+INSERT INTO purchases VALUES ('001', 'Handheld Portal Device', 'A', '2012-01-01', 100, 20, 100, '1234');
+INSERT INTO purchases VALUES ('002', 'Replusion Gel', 'A', '2012-01-01', 100, 20, 100, '1234');
+INSERT INTO purchases VALUES ('003', 'Propulsion Gel', 'D', '2012-01-01', 100, 20, 100, '2345');
+INSERT INTO purchases VALUES ('004', 'Conversion Gel', 'A', '2012-01-01', 100, 20, 100, '2345');
+INSERT INTO purchases VALUES ('005', 'Weighted Storage Cube', 'D', '2012-01-01', 100, 20, 100, '2345');
+INSERT INTO purchases VALUES ('006', 'Weighted Companion Cube', 'A', '2012-01-01', 100, 20, 100, '3456');
+INSERT INTO purchases VALUES ('007', '1500 Megawatt Heavy Duty Super-Colliding Super Button', 'A', '2012-01-01', 100, 20, 100, '3456');
+INSERT INTO purchases VALUES ('008', 'Edgeless Safety Cube Receptacle', 'D', '2012-01-01', 100, 20, 100, '1234');
+INSERT INTO purchases VALUES ('009', 'Safety Cube', 'D', '2012-01-01', 100, 20, 100, '1234');
+INSERT INTO purchases VALUES ('010', 'Weighted Pivot Cube', 'A', '2012-01-01', 100, 20, 100, '1234');
+INSERT INTO purchases VALUES ('011', 'Thermal Discouragement Beam Generator', 'A', '2012-01-01', 100, 20, 100, '2345');
+INSERT INTO purchases VALUES ('012', 'High Energy Pellet', 'A', '2012-01-01', 100, 20, 100, '3456');
